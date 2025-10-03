b92826f094ac592a9cae46dd4d4d5a475be7a9dc
Joost Lubach <joostlubach@gmail.com>
2025-10-03 09:36:11 +0200

    fix in nullable / default indexs

--- src/column.ts
+++ src/column.ts
@@ -238,71 +238,81 @@ export class Column<T extends z.ZodType<any>, Generated extends boolean = false>
 
 export class DefaultColumn<C extends Column<z.ZodType<any>, boolean>> extends Column<z.ZodDefault<C['zod']>> {
 
-  constructor(private readonly base: C, value: z.output<C['zod']> | (() => z.util.NoUndefined<z.output<C['zod']>>)) {
+  constructor(
+    base: C, value: z.output<C['zod']> | (() => z.util.NoUndefined<z.output<C['zod']>>)
+  ) {
     super(base.zod.default(value), base.options)
+    this._base = base
   }
+
+  private _base: C
+  public get base() { return this._base }
 
   public index(options?: IndexOptions): this
   public index(name: string, options?: IndexOptions): this
   public index(...args: any[]): this {
-    this.base.index(...args)
+    this._base = this._base.index(...args)
     return this
   }
 
   public unique(options?: UniqueOptions): this
   public unique(name: string, options?: UniqueOptions): this
   public unique(...args: any[]): this {
-    this.base.index(...args)
+    this._base = this._base.index(...args)
     return this
   }
 
   public get fieldType() {
-    return this.base.fieldType
+    return this._base.fieldType
   }
 
   public buildFieldDecorator(field: string, options: ColumnOptions = {}): PropertyDecorator {
-    return this.base.buildFieldDecorator(field, options)
+    return this._base.buildFieldDecorator(field, options)
   }
 
   public buildClassDecorator(field: string, options: ColumnOptions = {}): ClassDecorator {
-    return this.base.buildClassDecorator(field, options)
+    return this._base.buildClassDecorator(field, options)
   }
 
 }
 
 export class NullableColumn<C extends Column<z.ZodType<any>, boolean>> extends Column<z.ZodNullable<C['zod']>> {
 
-  constructor(private readonly base: C) {
+  constructor(base: C) {
     super(base.zod.nullable(), {
       ...base.options,
       nullable: true,
     })
+    this._base = base
   }
+
+  private _base: C
+  public get base() { return this._base }
 
   public index(options?: IndexOptions): this
   public index(name: string, options?: IndexOptions): this
   public index(...args: any[]): this {
-    this.base.index(...args)
+    this._base = this._base.index(...args)
     return this
   }
 
   public unique(options?: UniqueOptions): this
   public unique(name: string, options?: UniqueOptions): this
   public unique(...args: any[]): this {
-    this.base.index(...args)
+    this._base = this._base.index(...args)
     return this
   }
 
   public get fieldType() {
-    return this.base.fieldType
+    return this._base.fieldType
   }
 
   public buildFieldDecorator(field: string, options: ColumnOptions = {}): PropertyDecorator {
-    return this.base.buildFieldDecorator(field, {...options, nullable: true})
+    return this._base.buildFieldDecorator(field, {...options, nullable: true})
   }
 
   public buildClassDecorator(field: string, options: ColumnOptions = {}): ClassDecorator {
-    return this.base.buildClassDecorator(field, {...options, nullable: true})
+    return this._base.buildClassDecorator(field, {...options, nullable: true})
   }
 
 }
